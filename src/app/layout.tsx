d6fdbb5f3a36d347b18c35e434fb3a7bf28c3980
Hannah Nguyen <myhanh.nguyen.969317598@gmail.com>
2024-06-19 11:40:31 +0300

    sss

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import { GoogleTagManager } from "@next/third-parties/google";
+import Script from "next/script";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -17,19 +18,28 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      {process.env.NODE_ENV === "production" ? (
-        <GoogleTagManager gtmId="GTM-K5JMP2MK" />
-      ) : null}
-      <head></head>
+      <GoogleTagManager gtmId="GTM-K5JMP2MK" />
 
-      <noscript>
-        <iframe
+      <head></head>
+      <Script id="gtm" strategy="afterInteractive">
+        {`
+        (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-K5JMP2MK')
+ `}
+      </Script>
+      <noscript
+        dangerouslySetInnerHTML={{
+          __html: ` <iframe
           src="https://www.googletagmanager.com/ns.html?id=GTM-K5JMP2MK"
           height="0"
           width="0"
           style={{ display: "none", visibility: "hidden" }}
-        ></iframe>
-      </noscript>
+        />`,
+        }}
+      />
 
       <body className={inter.className}>{children}</body>
     </html>
